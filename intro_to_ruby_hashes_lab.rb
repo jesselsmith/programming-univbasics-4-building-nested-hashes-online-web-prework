956399f672ba06ec8ebe3b86b32aeb23a87f602d
Jesse Smith <jesse.lee.smith@gmail.com>
2019-09-23 22:45:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_fourth_tier
   fourth_tier =  monopoly_with_third_tier
   fourth_tier[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
   fourth_tier[:railroads][:names][:pennsylvania_railroad]["mortgage_value"] = "$200"
-  fourth_tier[:railroads][:names][:b_and_o_railroad]["mortgage_value"] = "$100"
-  fourth_tier[:railroads][:names][:shortline_railroad]["mortgage_value"] = "$100"
+  fourth_tier[:railroads][:names][:b_and_o_railroad]["mortgage_value"] = "$400"
+  fourth_tier[:railroads][:names][:shortline_railroad]["mortgage_value"] = "$800"
   fourth_tier
 end
